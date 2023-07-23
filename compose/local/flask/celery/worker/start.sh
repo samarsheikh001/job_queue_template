d4d710717716be99d4b7edd7fda84eec35fe27c8
root <root@dell.localdomain>
2023-07-23 16:43:50 +0530

    unverified commits

--- compose/local/flask/celery/worker/start.sh
+++ compose/local/flask/celery/worker/start.sh
@@ -3,4 +3,8 @@
 set -o errexit
 set -o nounset
 
-celery -A app.celery worker --loglevel=info
+# celery -A app.celery worker --loglevel=info
+
+watchfiles \
+  --filter python \
+  'celery -A app.celery worker --loglevel=info'
